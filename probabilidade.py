35a178509ca532f98c878f77b3a16956cd8abad3
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-10-07 18:17:42 -0300

    Criando a função 'funcao_variavel_aleatoria_distribuicao_poisson'.

--- probabilidade.py
+++ probabilidade.py
@@ -7,3 +7,7 @@ def funcao_variavel_aleatoria_distribuicao_binomial(n, p, x):
 
 def funcao_variavel_aleatoria_distribuicao_hipergeometrica(N, A, n, x):
     return math.comb(A, x) * math.comb(N - A, n - x) / math.comb(N, n)
+
+
+def funcao_variavel_aleatoria_distribuicao_poisson(l, x):
+    return (math.pow(math.e, (-1 * l)) * math.pow(l, x)) / math.factorial(x)
